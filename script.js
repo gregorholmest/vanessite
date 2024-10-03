e4deda990742948d3db5ebc00f32ced6623424b4
gregorholmest <gregorht@gmail.com>
2024-10-03 18:33:20 -0300

    1 commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('.navbar-nav .nav-link').forEach(link => {
+    link.addEventListener('click', function() {
+      const navbarCollapse = document.querySelector('.navbar-collapse');
+      if (navbarCollapse.classList.contains('show')) {
+        new bootstrap.Collapse(navbarCollapse).hide();
+      }
+    });
+  });
